09f3f79cd67d69582b5f465a061c8ddc08b7af7d
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-03-31 01:49:05 +0200

    make it scaffold and body in center

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,3 +9,10 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body:  Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child:  Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            
