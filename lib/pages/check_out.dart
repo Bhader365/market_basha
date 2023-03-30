f2015a89a0b68c46adcf223988899e4897626811
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-03-31 01:45:17 +0200

    some of libs we need it for login page

--- lib/pages/check_out.dart
+++
@@ -1,5 +0,0 @@
-import 'package:app1/provider/cart.dart';
-import 'package:app1/widgets/row_content.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../widgets/constant_colors.dart';
---
+++ lib/pages/login.dart
@@ -0,0 +1,5 @@
+import 'package:app1/pages/home.dart';
+import 'package:app1/pages/register.dart';
+import 'package:app1/widgets/constant_colors.dart';
+import 'package:app1/widgets/text_field.dart';
+import 'package:flutter/material.dart';
