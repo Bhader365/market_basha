188479519beb3c11c28c1d0bda49c5d3f7b6d7d2
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-03-31 01:46:21 +0200

    create class of  login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,3 +3,6 @@ import 'package:app1/pages/register.dart';
 import 'package:app1/widgets/constant_colors.dart';
 import 'package:app1/widgets/text_field.dart';
 import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
