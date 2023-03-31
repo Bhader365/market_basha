02613bb729a9c7d5aec63ff1065400944b63026f
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-01 00:45:29 +0200

    make class of text field

--- lib/constant/text_field.dart
+++ lib/constant/text_field.dart
@@ -1,1 +1,6 @@
 import 'package:flutter/material.dart';
+
+
+// ignore: camel_case_types
+class Text_field extends StatelessWidget {
+  const Text_field({super.key, this.onsaved, required this.hint, required this.secure, required this.type });
