21cc6bd8c9f7448ed9d2a8bcc59f8ef0017690fc
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-01 00:46:18 +0200

    some of const variables

--- lib/constant/text_field.dart
+++ lib/constant/text_field.dart
@@ -4,3 +4,9 @@ import 'package:flutter/material.dart';
 // ignore: camel_case_types
 class Text_field extends StatelessWidget {
   const Text_field({super.key, this.onsaved, required this.hint, required this.secure, required this.type });
+
+final TextInputType type;
+final bool secure;
+final String hint ;
+final void Function(String?)? onsaved;
+  @override
