ded59d2017ee1243f4f13cdb76f73ac2ddc99bec
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-03 19:36:15 +0200

    list of items

--- lib/constant/items.dart
+++ lib/constant/items.dart
@@ -5,3 +5,7 @@ class item {
   String name;
   item({required this.imgpath,required this.name,required this.price ,this.location="Main Branch"});
 }
+
+final List<item> items=[
+  item(name: "product1", imgpath:"assets/img/1.webp" , price:152.5, location: "flower shop"),
+  
