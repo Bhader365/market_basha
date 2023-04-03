3f1c520ea77862fd4214a32747629271dd3cc007
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-03 19:35:04 +0200

    class of items datatype of data in page

---
+++ lib/constant/items.dart
@@ -0,0 +1,7 @@
+class item {
+  String imgpath;
+  double price;
+  String location;
+  String name;
+  item({required this.imgpath,required this.name,required this.price ,this.location="Main Branch"});
+}
