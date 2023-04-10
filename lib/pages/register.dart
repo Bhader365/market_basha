f03661f59842cc207e8856ee6b447137735dda21
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-10 06:00:59 +0200

    class of register for page

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -3,3 +3,6 @@ import 'package:app1/widgets/text_field.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/constant_colors.dart';
+
+class Regiser extends StatelessWidget {
+  const Regiser({super.key});
