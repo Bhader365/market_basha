09ed09a32e3a21162705653459793f2257312e03
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-10 06:53:00 +0200

    appBar for page

--- lib/pages/homer.dart
+++ lib/pages/homer.dart
@@ -2,3 +2,9 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final classInstancee = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
