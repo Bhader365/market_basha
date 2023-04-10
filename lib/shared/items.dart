3cfb0c51aaa6d841e4b19852c0bd0755729c6f49
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-10 22:38:37 +0200

    data of 2nd item

--- lib/shared/items.dart
+++ lib/shared/items.dart
@@ -9,4 +9,5 @@ class item {
 
 final List<item> items=[
   item(name: "product1", imgpath:"assets/img/1.webp" , price:152.5, location: "flower shop"),
+  item(name: "product2", imgpath:"assets/img/2.webp" , price:154.5,),
   
