713a3195d4408785701e57bf6a136355b2767eb2
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-10 06:53:48 +0200

    some of imports

--- lib/pages/homer.dart
+++ lib/pages/homer.dart
@@ -1,4 +1,11 @@
+import 'package:app1/pages/details.dart';
+import 'package:app1/provider/cart.dart';
+import 'package:app1/widgets/constant_colors.dart';
+import 'package:app1/widgets/row_content.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/item.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
