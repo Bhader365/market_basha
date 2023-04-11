0c54ac5c327f42d42b70e6a6192f25f881bf3d68
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-11 13:08:40 +0200

    class of checkout of product

---
+++ lib/pages/check_out.dart
@@ -0,0 +1,8 @@
+import 'package:app1/provider/cart.dart';
+import 'package:app1/widgets/row_content.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../widgets/constant_colors.dart';
+
+class Checkout extends StatelessWidget {
+  const Checkout({super.key});
