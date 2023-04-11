111380ee3980672ded93b1922393fabf1a2a9c3d
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-11 12:45:15 +0200

    class content

--- lib/shared/row_content.dart
+++ lib/shared/row_content.dart
@@ -2,3 +2,6 @@ import 'package:app1/pages/check_out.dart';
 import 'package:app1/provider/cart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+class Content extends StatelessWidget {
+  const Content({super.key});
