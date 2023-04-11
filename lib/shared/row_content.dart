e2fcc76e2bcc84d85de1eb93f1433c22f888538c
Bhader365 <127336760+Bhader365@users.noreply.github.com>
2023-04-11 12:46:35 +0200

    stack and some of data style

--- lib/shared/row_content.dart
+++ lib/shared/row_content.dart
@@ -5,3 +5,24 @@ import 'package:provider/provider.dart';
 
 class Content extends StatelessWidget {
   const Content({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final classInstancee = Provider.of<Cart>(context);
+    return  Row(
+      children: [
+        Stack(
+          children: [
+            Positioned(
+              bottom: 25,
+              left: 2,
+              child: Container(
+                  padding: const EdgeInsets.all(3),
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(211, 164, 255, 193),
+                      shape: BoxShape.circle),
+                  child: Text(
+                    "${classInstancee.selectitems.length}",
+                    style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                  )),
+            ),
